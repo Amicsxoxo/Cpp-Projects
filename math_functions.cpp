1a2fa4d39638e6c71b305104e5a9aae6ab6a4cc2
Amicsxoxo <onwukanjo.john@gmail.com>
2026-08-10 18:41:34 +0100

    I did math functions, and did a practice

--- math_functions.cpp
+++ math_functions.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cmath>
 
 int main(){
+
+  double x = 3.9;
+  double y = 4;
+  double z;
+
+  // z = std::max(x, y); // Used to return the max value in a comparison
+  // z = std::min(x, y); // Used to return the minimum value in a comparison
+  // z = pow(2, 3); // Used to raise the first value by the second value
+  // z = sqrt(9); //Used to get the square root of a number
+  // z = abs(-3); // Used to know how far the number is from 0
+  // z = round(x); //Used to round numbers
+  // z = ceil(); //Used to round numbers up to highest number
+  z = floor(x); //Used to round numbers down
+
+  std::cout << z;
 
   return 0;
 }
