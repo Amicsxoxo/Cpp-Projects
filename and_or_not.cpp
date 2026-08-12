87d192b69ad5e10f3c3f02110dbd14e82f52ced3
Amicsxoxo <onwukanjo.john@gmail.com>
2026-08-12 22:01:56 +0100

    finished logical operations, temperation conversion project and started string operations:

--- and_or_not.cpp
+++ and_or_not.cpp
@@ -7,16 +7,24 @@ int main(){
   // ! == NOT
 
   int temp;
+  bool sunny = false;
+
   std::cout << "Enter the temperature: ";
   std::cin >> temp;
 
   if (temp <= 0 || temp >= 30){
-    std::cout << "The temperature is bad!!";
+    std::cout << "The temperature is bad!!\n";
   }
   else{
-    std::cout << "The temperature is good!!";
+    std::cout << "The temperature is good!!\n";
   }
   
+  if(!sunny){
+    std::cout << "It is cloudy outside";
+  }
+  else{
+    std::cout << "It is sunny outside";
+  }
 
   return 0;
 }
