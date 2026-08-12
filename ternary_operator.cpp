4a1bdfff0ddd6f8066ed8a8c0db69547009af646
Amicsxoxo <onwukanjo.john@gmail.com>
2026-08-12 20:54:06 +0100

    Learnt about teneary operators and started and, or and not

--- ternary_operator.cpp
+++ ternary_operator.cpp
@@ -4,8 +4,16 @@ int main(){
   //It is used to replace the if else statements 
   //condition ? expression1: expression2
 
-  int grade = 75;
+  // int grade = 50;
+  // grade >= 50 ? std::cout << "You pass" : std::cout << "You falil";
 
+  // int num = 9;
+  // num % 2 == 1 ? std::cout << "ODD" : std::cout << "EVEN";
+
+  bool hungry = true;
+
+  // hungry ? std::cout << "I'm Hungry !!" : std::cout << "I'm not hungry";
+  std::cout << (hungry ? "I'm hungry !!" : "I'm not hungry");
 
   return 0;
 }
